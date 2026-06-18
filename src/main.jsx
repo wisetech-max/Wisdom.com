4407e8b8f8feeb539b3ac1147a45f0a1dd27490c
wisetech-max <onyedikauwakwe@gmail.com>
2026-06-18 23:07:31 +0100

    Move theme toggle into mobile hamburger menu

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ThemeContext, ThemeProvider } from "./Context/ThemeProvider.jsx";
 import "./index.css";
+import { injectSpeedInsights } from "@vercel/speed-insights";
+
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
